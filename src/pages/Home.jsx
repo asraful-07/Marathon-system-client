9f1a1859436fadc7ca42a14a078623b5ca35ad1b
asraful-07 <asraful0508@gmail.com>
2024-12-22 17:35:06 +0600

    added marathon cards home add the section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Banner";
+import Marathons from "../components/Marathons";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Marathons />
     </div>
   );
 };
