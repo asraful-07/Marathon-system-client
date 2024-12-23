5f1bd9daac52d7128322b4cbe615b098c7b7d349
asraful-07 <asraful0508@gmail.com>
2024-12-23 08:50:18 +0600

    marathon details page done

--- src/components/MarathonDetails.jsx
+++ src/components/MarathonDetails.jsx
@@ -14,18 +14,13 @@ const MarathonDetails = () => {
     totalRegistrations,
   } = marathon;
 
-  // Check if registration is open
-  const isRegistrationOpen =
-    new Date(startRegistrationDate) <= new Date() &&
-    new Date(endRegistrationDate) >= new Date();
-
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-4">{title}</h1>
       <img
         src={image}
         alt={title}
-        className="w-full h-60 object-cover mb-4 rounded"
+        className="max-w-4xl h-60 object-cover mb-4 rounded"
       />
       <p>
         <strong>Description:</strong> {description}
@@ -38,15 +33,6 @@ const MarathonDetails = () => {
       <p>
         <strong>Total Registrations:</strong> {totalRegistrations}
       </p>
-
-      {/* Registration status */}
-      {isRegistrationOpen ? (
-        <p className="text-green-500 mt-4">Registration is open!</p>
-      ) : (
-        <p className="text-red-500 mt-4">Registration is currently closed.</p>
-      )}
-
-      {/* Registration Button */}
     </div>
   );
 };
