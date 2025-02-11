eca52200002f8ee145a91c3eae3abf12c4fa8c11
asraful-07 <asraful0508@gmail.com>
2025-02-11 16:14:51 +0600

    grid col 4 done

--- src/components/Marathons.jsx
+++ src/components/Marathons.jsx
@@ -32,7 +32,7 @@ const Marathons = () => {
       </div>
 
       {/* Marathon Cards */}
-      <div className="container mx-auto grid grid-cols-1 gap-8 mt-10 md:grid-cols-2 lg:grid-cols-3">
+      <div className="container mx-auto grid grid-cols-1 gap-8 mt-10 md:grid-cols-2 lg:grid-cols-4">
         {marathons.map((marathon) => (
           <MarathonCards key={marathon._id} marathon={marathon} />
         ))}
